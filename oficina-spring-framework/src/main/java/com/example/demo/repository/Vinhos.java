35cbfc69538a5b8fa8e3fa13151cf39b6d645618
TitaniumCortez <38405839+TitaniumCortez@users.noreply.github.com>
2018-04-17 11:38:01 -0300

    Add files via upload

---
+++ oficina-spring-framework/src/main/java/com/example/demo/repository/Vinhos.java
@@ -0,0 +1,10 @@
+package com.example.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.demo.model.Vinho;
+
+public interface Vinhos extends JpaRepository<Vinho,Long> {
+
+
+
+}
